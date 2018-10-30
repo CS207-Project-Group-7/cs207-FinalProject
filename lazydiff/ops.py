d6176e48806b9c95cfe56d2b5cd503ac8efef08f
Yunbin Zhang <mzhangyb@gmail.com>
2018-10-29 23:03:01 -0400

    added other ops

--- lazydiff/ops.py
+++ lazydiff/ops.py
@@ -31,12 +31,66 @@ def atan(var):
     result.parents.append((1 / math.sqrt(1 - var.val ** 2), var))
     return result
 
+def arcsin(var):
+    return asin(var)
+
+def arccos(var):
+    return acos(var)
+
+def arctan(var):
+    return atan(var)
+
+def sinh(var):
+    result = vars.Scalar(math.sinh(var.val))
+    result.parents.append((math.cosh(var.val), var))
+    return result
+
+def cosh(var):
+    result = vars.Scalar(math.cosh(var.val))
+    result.parents.append((math.sinh(var.val), var))
+    return result
+
+def tanh(var):
+    result = vars.Scalar(math.tanh(var.val))
+    result.parents.append((1 / (math.cosh(var.val)**2), var))
+    return result
+
+def asinh(var):
+    result = vars.Scalar(math.asinh(var.val))
+    result.parents.append((1 / math.sqrt(var.val ** 2 + 1), var))
+    return result
+
+def acosh(var):
+    result = vars.Scalar(math.acosh(var.val))
+    result.parents.append((1 / (math.sqrt(var.val-1) * math.sqrt(var.val+1)), var))
+    return result
+
+def atanh(var):
+    result = vars.Scalar(math.atanh(var.val))
+    result.parents.append((1/ (1 - var.val ** 2), var))
+    return result
+
+def arcsinh(var):
+    return asinh(var)
+
+def arccosh(var):
+    return acosh(var)
+
+def arctanh(var):
+    return atanh(var)
+
 def exp(var):
     result = vars.Scalar(math.exp(var.val))
     result.parents.append((math.exp(var.val), var))
     return result
 
-def log(var):
-    result = vars.Scalar(math.log(var.val))
-    result.parents.append((1 / var.val, var))
+def log(var, base = math.e):
+    result = vars.Scalar(math.log(var.val, base))
+    result.parents.append((1 / (var.val * math.log(base)), var))
     return result
+
+def power(var, exponent):
+    return var**exponent
+
+
+
