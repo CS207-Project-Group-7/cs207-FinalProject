46e8f6509e8674396cfd3ccb71d33f2f7bbfd500
Yunbin Zhang <mzhangyb@gmail.com>
2018-10-29 21:53:06 -0400

    added support for vectors

--- lazydiff/vars.py
+++ lazydiff/vars.py
@@ -1,4 +1,7 @@
 import math
+import numpy as np
+import collections
+import numbers
 
 class Scalar:
     def __init__(self, val):
@@ -76,16 +79,77 @@ class Scalar:
 
 class Vector:
     def __init__(self, *args):
-        self._components = args
-        self.val = tuple([component.val for component in self._components])
+        if (isinstance(args[0],Scalar)):
+            self._components = args
+            self.val = tuple([component.val for component in self._components])
+        # support list or ndarray input
+        elif ((isinstance(args[0],collections.Sequence) or isinstance(args[0],np.ndarray)) and len(args)==1):
+            self._components = tuple(args[0])
+            self.val = tuple([component.val for component in self._components])
+        else:
+            raise TypeError("inputs need to be Scalar objects or a sequence of Scalar objects")
 
     def grad(self, *args):
-        return tuple([component.grad(*args) for component in self._components])
-
+        if (args == ()): 
+            return
+        if (isinstance(args[0],Scalar)):
+            return tuple([component.grad(*args) for component in self._components])
+        # elementwise or all components?
+        elif (isinstance(args[0],Vector) and len(args)==1):
+            # elementwise
+            # return tuple([comp1.grad(comp2) for comp1, comp2 in zip(self._components, args[0]._components)])
+            # Hessian
+            return tuple([comp1.grad(*args[0]) for comp1 in self._components])
     def __getitem__(self, ind):
         if ind not in range(self.__len__()):
             raise IndexError
         return self._components[ind]
 
     def __len__(self):
-        return len(self._component)
+        return len(self._components)
+
+    def _check_broadcast(self, other):
+        if (len(self) != len(other)):
+            raise ValueError("operands could not be broadcast together with lengths {} and {}".format(len(self),len(other)))
+
+    def __neg__(self):
+        return Vector([-component for component in self._components])
+    
+    def __add__(self, other):
+        if (isinstance(other, numbers.Number) or isinstance(other, Scalar)):
+            return Vector([component + other for component in self._components])
+        elif (isinstance(other, Vector)):
+            self._check_broadcast(other)
+            return Vector([comp1+comp2 for comp1, comp2 in zip(self._components, other._components)])
+        else:
+            raise TypeError("input needs to be a numeric value or Vector object")
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        return self.__add__(other.__neg__())
+
+    def __rsub__(self, other):
+        return self.__neg__().__add__(other)
+
+    def __mul__(self, other):
+        if (isinstance(other, Scalar)):
+            return Vector([component*other for component in self._components])
+        elif (isinstance(other, Vector)):
+            self._check_broadcast(other)
+            return Vector([comp1*comp2 for comp1, comp2 in zip(self._components, other._components)])
+        else:
+            raise TypeError("input needs to be Scalar or Vector object")
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        return self.__mul__(other.__pow__(-1))
+
+    def __rtruediv__(self, other):
+        return self.__pow__(-1).__mul__(other)
+
+    def __pow__(self, other):
+        return Vector([component**other for component in self._components])
