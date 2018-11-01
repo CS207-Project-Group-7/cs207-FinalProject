ae4abd1eac5f6b07acdad4efcf5aa8d92f545a3a
Raymond Lin <raylin1000@gmail.com>
2018-11-01 19:53:26 -0400

    Add some docstrings for Scalar class and ops module

--- lazydiff/ops.py
+++ lazydiff/ops.py
@@ -2,89 +2,150 @@ import math
 import lazydiff.vars as vars
 
 def sin(var):
+    """
+    Returns variable representing sin applied to the input variable var
+    """
     result = vars.Scalar(math.sin(var.val))
     result.parents.append((math.cos(var.val), var))
     return result
 
 def cos(var):
+    """
+    Returns variable representing cos applied to the input variable var
+    """
     result = vars.Scalar(math.cos(var.val))
     result.parents.append((-math.sin(var.val), var))
     return result
 
 def tan(var):
+    """
+    Returns variable representing tan applied to the input variable var
+    """
     result = vars.Scalar(math.tan(var.val))
     result.parents.append((1 / math.cos(var.val) ** 2, var))
     return result
 
 def asin(var):
+    """
+    Returns variable representing asin applied to the input variable var
+    """
     result = vars.Scalar(math.asin(var.val))
     result.parents.append((1 / math.sqrt(1 - var.val ** 2), var))
     return result
 
 def acos(var):
+    """
+    Returns variable representing acos applied to the input variable var
+    """
     result = vars.Scalar(math.atan(var.val))
     result.parents.append((1 / (var.val ** 2 + 1), var))
     return result
 
 def atan(var):
+    """
+    Returns variable representing atan applied to the input variable var
+    """
     result = vars.Scalar(math.asin(var.val))
     result.parents.append((1 / math.sqrt(1 - var.val ** 2), var))
     return result
 
 def arcsin(var):
+    """
+    Wrapper function for asin
+    """
     return asin(var)
 
 def arccos(var):
+    """
+    Wrapper function for asin
+    """
     return acos(var)
 
 def arctan(var):
+    """
+    Wrapper function for atan
+    """
     return atan(var)
 
 def sinh(var):
+    """
+    Returns variable representing sinh applied to the input variable var
+    """
     result = vars.Scalar(math.sinh(var.val))
     result.parents.append((math.cosh(var.val), var))
     return result
 
 def cosh(var):
+    """
+    Returns variable representing cosh applied to the input variable var
+    """
     result = vars.Scalar(math.cosh(var.val))
     result.parents.append((math.sinh(var.val), var))
     return result
 
 def tanh(var):
+    """
+    Returns variable representing tanh applied to the input variable var
+    """
     result = vars.Scalar(math.tanh(var.val))
     result.parents.append((1 / (math.cosh(var.val) ** 2), var))
     return result
 
 def asinh(var):
+    """
+    Returns variable representing asinh applied to the input variable var
+    """
     result = vars.Scalar(math.asinh(var.val))
     result.parents.append((1 / math.sqrt(var.val ** 2 + 1), var))
     return result
 
 def acosh(var):
+    """
+    Returns variable representing acosh applied to the input variable var
+    """
     result = vars.Scalar(math.acosh(var.val))
     result.parents.append((1 / math.sqrt(var.val ** 2 - 1), var))
     return result
 
 def atanh(var):
+    """
+    Returns variable representing atanh applied to the input variable var
+    """
     result = vars.Scalar(math.atanh(var.val))
     result.parents.append((1 / (1 - var.val ** 2), var))
     return result
 
 def arcsinh(var):
+    """
+    Wrapper function for asinh
+    """
     return asinh(var)
 
 def arccosh(var):
+    """
+    Wrapper function for acosh
+    """
     return acosh(var)
 
 def arctanh(var):
+    """
+    Wrapper function for atanh
+    """
     return atanh(var)
 
 def exp(var):
+    """
+    Returns variable representing exp applied to the input variable var
+    """
     result = vars.Scalar(math.exp(var.val))
     result.parents.append((math.exp(var.val), var))
     return result
 
 def log(var, base=math.e):
+    """
+    Returns variable representing log applied to the input variable var.
+    Base of log is optional with default base e
+    """
     try:
         result = vars.Scalar(math.log(var.val, base.val))
         result.parents.append((1 / (var.val * math.log(base.val)), var))
@@ -95,22 +156,46 @@ def log(var, base=math.e):
     return result
 
 def neg(var):
+    """
+    Wrapper function for __neg__
+    """
     return -var
 
 def add(var1, var2):
+    """
+    Wrapper function for __add__ and __radd__
+    """
     return var1 + var2
 
 def sub(var1, var2):
+    """
+    Wrapper function for __sub__ and __rsub__
+    """
     return var1 - var2
 
 def mul(var1, var2):
+    """
+    Wrapper function for __mul__ and __rmul__
+    """
     return var1 * var2
 
 def div(var1, var2):
+    """
+    Wrapper function for __truediv__ and __rtruediv__
+    """
     return var1 / var2
 
 def pow(var1, var2):
+    """
+    Wrapper function for __pow__
+    """
     return var1 ** var2
+
+def abs(var):
+    """
+    Wrapper function for __abs__
+    """
+    return abs(var)
 
 
 
--- lazydiff/vars.py
+++ lazydiff/vars.py
@@ -1,20 +1,34 @@
 import math
 
 class Scalar:
+    """
+    A class for lazydiff autograd scalar variables.
+    """
+
     def __init__(self, val):
+        """
+        Initializes Scalar object with numerical value val.
+        """
         self.val = val
         self._grad_cache = {self: 1}
         self.parents = []
     
     def grad(self, *args):
+        """
+        Returns tuple representing gradient with respect to each variable
+        provided as arguments.
+        """
         result = len(args) * [0]
         for i, var in enumerate(args):
-            if var not in self._grad_cache:
-                self._compute_grad(var)
+            self._compute_grad(var)
             result[i] = self._grad_cache[var]
         return tuple(result)
     
     def _compute_grad(self, var):
+        """
+        Internal method for computing gradient with respect to variable var
+        and storing the result in the gradient cache dictionary.
+        """
         if var not in self._grad_cache:
             grad = 0
             for val, parent_var in self.parents:
@@ -23,11 +37,18 @@ class Scalar:
             self._grad_cache[var] = grad
 
     def __neg__(self):
+        """
+        Returns Scalar object representing negation of a Scalar object.
+        """
         result = Scalar(-self.val)
         result.parents.append((-1., self))
         return result
     
     def __add__(self, other):
+        """
+        Returns Scalar object representing addition of two Scalar objects or
+        the addition of a Scalar object and a Python number.
+        """
         try:
             result = Scalar(self.val + other.val)
             result.parents.append((1., other))
@@ -37,15 +58,31 @@ class Scalar:
         return result
 
     def __radd__(self, other):
+        """
+        Returns Scalar object representing right addition of a Scalar object
+        with a Python number.
+        """
         return self.__add__(other)
 
     def __sub__(self, other):
+        """
+        Returns Scalar object representing subtraction of two Scalar objects or
+        the subtraction of a Scalar object and a Python number
+        """
         return self.__add__(other.__neg__())
 
     def __rsub__(self, other):
+        """
+        Returns Scalar object representing right subtraction of a Scalar object
+        with a Python number
+        """
         return self.__neg__().__add__(other)
 
     def __mul__(self, other):
+        """
+        Returns Scalar object representing multiplication of two Scalar objects 
+        or the multiplication of a Scalar object and a Python number
+        """
         try:
             result = Scalar(self.val * other.val)
             result.parents.append((other.val, self))
@@ -56,45 +93,92 @@ class Scalar:
         return result
     
     def __rmul__(self, other):
+        """
+        Returns Scalar object representing right multiplication of a Scalar 
+        object with a Python number
+        """
         return self.__mul__(other)
 
     def __truediv__(self, other):
+        """
+        Returns Scalar object representing division of two Scalar objects or 
+        the division of a Scalar object and a Python number
+        """
         return self.__mul__(other.__pow__(-1))
 
     def __rtruediv__(self, other):
+        """
+        Returns Scalar object representing right division of a Scalar object
+        with a Python number
+        """
         return self.__pow__(-1).__mul__(other)
 
     def __pow__(self, other):
+        """
+        Returns Scalar object representing exponentiation of two Scalar objects 
+        or the exponentiation of a Scalar object and a Python number
+        """
         try:
-            result = Scalar(math.pow(self.val, other.val))
-            result.parents.append((other.val * math.pow(self.val, other.val - 1), self))
-            result.parents.append((math.pow(self.val, other.val) * math.log(self.val), other))
+            result = Scalar(self.val ** other.val)
+            result.parents.append((other.val * self.val ** (other.val - 1), self))
+            result.parents.append((math.log(self.val) * self.val ** other.val, other))
         except AttributeError:
             result = Scalar(math.pow(self.val, other))
-            result.parents.append((other * math.pow(self.val, other - 1), self))
+            result.parents.append((other * self.val ** (other - 1), self))
+        return result
+
+    def __rpow__(self, other):
+        """
+        Returns Scalar object representing right exponentiation of a Scalar 
+        object with a Python number
+        """
+        result = Scalar(other ** self.val)
+        result.parents.append((math.log(other) * other ** self.val, self))
         return result
 
     def __abs__(self):
+        """
+        Returns Scalar object representing absolute value of a Scalar object
+        """
         result = Scalar(abs(self.val))
-        result.parents.append((self.val/result.val, self))
+        result.parents.append((self.val / abs(self.val), self))
         return result
 
     def _ban_augmented_assignment(self):
-        raise Exception("In-place operations are not supported for lazydiff variables.")
+        """
+        Internal method for raising error when in-place operation with Scalar
+        objects is attempted.
+        """
+        raise TypeError("In-place operations are not supported for lazydiff variables.")
     
     def __iadd__(self, other):
+        """
+        Raises error when in-place addition is called
+        """
         self._ban_augmented_assignment()
 
     def __isub__(self, other):
+        """
+        Raises error when in-place subtraction is called
+        """
         self._ban_augmented_assignment()
 
     def __imul__(self, other):
+        """
+        Raises error when in-place multiplication is called
+        """
         self._ban_augmented_assignment()
 
     def __itruediv__(self, other):
+        """
+        Raises error when in-place division is called
+        """
         self._ban_augmented_assignment()
 
     def __ipow__(self, other):
+        """
+        Raises error when in-place exponentiation is called
+        """
         self._ban_augmented_assignment()
 
 class Vector:
