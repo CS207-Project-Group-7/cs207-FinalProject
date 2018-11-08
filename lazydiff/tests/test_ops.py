141228e558a07b7f301d53748668e122721a2146
Zheng Yang <zhengyang@g.harvard.edu>
2018-11-07 23:32:51 -0500

    test for ops_2

--- lazydiff/tests/test_ops.py
+++ lazydiff/tests/test_ops.py
@@ -17,67 +17,67 @@ def test_cos():
 	assert var2.grad(var1) == pytest.approx([0])
 
 def test_tan():
-	var1 = vars.Scalar(math.pi/4)
+	var1 = vars.Scalar(math.pi/4.)
 	var2 = ops.tan(var1)
 	assert var2.val == pytest.approx(1)
 	assert var2.grad(var1) == np.array([2.])
 
 def test_asin():
-	var1 = vars.Scalar(0)
+	var1 = vars.Scalar(0.)
 	var2 = ops.asin(var1)
 	assert var2.val == pytest.approx(0)
 	assert var2.grad(var1) == np.array([1])
 
 def test_acos():
-	var1 = vars.Scalar(1)
+	var1 = vars.Scalar(0.)
 	var2 = ops.acos(var1)
-	assert var2.val == pytest.approx(0)
-	assert var2.grad(var1) == np.array([-inf])
+	assert var2.val == pytest.approx(1.570796,abs=1e-2)
+	assert var2.grad(var1) == np.array([-1])
 
 def test_atan():
-	var1 = vars.Scalar(0)
+	var1 = vars.Scalar(0.)
 	var2 = ops.atan(var1)
 	assert var2.val == pytest.approx(0)
 	assert var2.grad(var1) == np.array([1])
 
 def test_sinh():
-	var1 = vars.Scalar(0)
+	var1 = vars.Scalar(0.)
 	var2 = ops.sinh(var1)
 	assert var2.val == pytest.approx(0)
 	assert var2.grad(var1) == np.array([1])
 
 def test_cosh():
-	var1 = vars.Scalar(0)
+	var1 = vars.Scalar(0.)
 	var2 = ops.cosh(var1)
 	assert var2.val == pytest.approx(1)
 	assert var2.grad(var1) == np.array([0])
 
 def test_tanh():
-	var1 = vars.Scalar(0)
+	var1 = vars.Scalar(0.)
 	var2 = ops.tanh(var1)
 	assert var2.val == pytest.approx(0)
 	assert var2.grad(var1) == np.array([1])
 
 def test_asinh():
-	var1 = vars.Scalar(0)
+	var1 = vars.Scalar(0.)
 	var2 = ops.asinh(var1)
 	assert var2.val == pytest.approx(0)
 	assert var2.grad(var1) == np.array([1])
 
 def test_acosh():
-	var1 = vars.Scalar(10)
+	var1 = vars.Scalar(10.)
 	var2 = ops.acosh(var1)
 	assert var2.val == pytest.approx(2.993, abs=1e-2)
 	assert var2.grad(var1) == np.array([0.10050378])
 
 def test_acosh():
-	var1 = vars.Scalar(10)
+	var1 = vars.Scalar(10.)
 	var2 = ops.acosh(var1)
 	assert var2.val == pytest.approx(2.99322, abs=1e-3)
-	assert var2.grad(var1) == np.array([1])
+	assert var2.grad(var1) == pytest.approx(0.100504, abs=1e-3)
 
 def test_atanh():
-	var1 = vars.Scalar(0)
+	var1 = vars.Scalar(0.)
 	var2 = ops.atanh(var1)
 	assert var2.val == pytest.approx(0)
 	assert var2.grad(var1) == np.array([1])
