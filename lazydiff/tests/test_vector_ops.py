2af776cec19e690f4893f1ab7fc4034a5349918d
Raymond Lin <raylin1000@gmail.com>
2018-12-11 23:27:14 -0500

    Fix approx in test and update requirements

--- lazydiff/tests/test_vector_ops.py
+++ lazydiff/tests/test_vector_ops.py
@@ -200,5 +200,5 @@ def test_composite_trig():
     x2 = ops.sin(x) / ops.cos(x)
     x3 = ops.tan(x)
     x.forward()
-    assert np.all(x2 == x3)
+    assert np.all(x2.val == pytest.approx(x3.val))
     assert np.all(x2.grad(x) == x3.grad(x))
